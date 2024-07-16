56d501712e1fd64f4318f1584482b4d7bb3ee598
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-07-16 15:16:59 +0200

    [UPD] migratios

---
+++ database/migrations/2024_07_16_122603_create_comics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 250);
+            $table->text('description');
+            $table->string('thumb', 250);
+            $table->string('price', 10);
+            $table->string('series', 250);
+            $table->date('sale_date');
+            $table->string('type', 50);
+            $table->json('artists')->nullable();
+            $table->json('writers')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
